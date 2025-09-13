f3a75cae976edb48f11e1361bd74d9d7d581165e
Gokulapriya S <23csa37@karpagamtech.ac.in>
2025-09-13 14:40:00 +0530

    app.js

--- shape generator/app.js
+++ shape generator/app.js
@@ -1,18 +1,21 @@
-function shapegeneration(){
-    const num=parseInt(document.getElementById("no-of-shape").value);
-    const place=document.getElementById("shape-generation");
-    const shape=document.querySelector('input[name="shape"]:checked').value;
-    for(let i=1;i<=num;i++){
-        const div=document.createElement('div');
-        if(shape=="circle"){
-            div.classList.add("circle");
-        }else if(shape=="square"){
-            div.classList.add("square");
-        }else if(shape=="rectangle"){
-            div.classList.add("rectangle");
-        }else{
-            alert("Please select a shape and enter a valid number!");
-        }
-        place.appendChild(div);
+const generateShapes = () => {
+const count = +document.querySelector("#no-of-shape").value;
+const container = document.querySelector("#shape-generation");
+const selected = document.querySelector("input[name='shape']:checked")?.value;
+for (let i = 0; i < count; i++) {
+const shapeDiv = document.createElement("div");
+   switch (selected) {
+      case "circle":
+        shapeDiv.className = "circle";
+        break;
+      case "square":
+        shapeDiv.className = "square";
+        break;
+      case "rectangle":
+        shapeDiv.className = "rectangle";
+        break;
     }
-}
+ container.appendChild(shapeDiv);
+  }
+};
+document.querySelector("#submit").addEventListener("click", generateShape
